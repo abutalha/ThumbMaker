e75d9775dce69c0defd72c5792b2943a654c490e
Salim <salim.meetme@gmail.com>
2022-08-09 05:38:04 +0300

    Initial Commit

---
+++ ThumbMaker/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ThumbMaker
+//
+//  Created by AbuTalha on 09/08/2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
